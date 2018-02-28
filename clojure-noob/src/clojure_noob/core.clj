92977e17264f484ecbb59b4919b30e7467d4bff1
Bradford D. Boyle <bradford.d.boyle@gmail.com>
2018-02-28 09:22:06 -0800

    A Clojure Crash Course

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -4,4 +4,50 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "I'm a little teapot"))
+
+(defn error-message
+  [severity]
+  (str "OH GOD! IT'S A DISASTER! WE'RE "
+       (if (= severity :mild)
+         "MILDLY INCONVENIENCED!"
+         "DOOOOOOOOOOMED!")))
+
+(def name "Chewbacca")
+(str "\"Uggllglglglglglglglll\" - " name)
+
+(defn too-enthusiastic
+  "Return a cheer that might be a bit too enthusiastic"
+  [name]
+  (str "OH. MY. GOD! " name " YOU ARE MOST DEFINITELY LIKE THE BEST "
+       "MAN SLASH WOMAN EVER I LOVE YOU AND WE SHOULD RUN AWAY SOMEWHERE"))
+
+(defn no-params
+  []
+  "I take no parameters!")
+
+(defn one-param
+  [x]
+  (str "I take one parameter : " x))
+
+(defn two-params
+  [x y]
+  (str "Two parameters! That's nothing! Pah! I will smoosh them "
+       "together to spite you! " x y))
+
+;; arity overloading
+(defn x-chop
+  "Describe the kind of chop your're inflicting on someone"
+  ([name chop-type]
+    (str "I " chop-type " chop " name "! Take that!"))
+  ([name]
+    (x-chop name "karate")))
+
+;; example of rest parameter
+(defn codger-communication
+  [whippersnapper]
+  (str "Get off my lawn, " whippersnapper "!!!"))
+
+(defn codger
+  [& whippersnappers]
+  (map codger-communication whippersnappers))
