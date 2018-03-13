91d2fc6e70aa45970be7401fb5af22d96d21c9cb
Bradford D. Boyle <bradford.d.boyle@gmail.com>
2018-03-13 10:04:28 -0700

    [WIP] End of chapter project

--- fwpd/src/fwpd/core.clj
+++ fwpd/src/fwpd/core.clj
@@ -1,7 +1,21 @@
-(ns fwpd.core
-  (:gen-class))
+(ns fwpd.core)
+(def filename "suspects.csv")
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int
+  [str]
+  (Integer. str))
+
+(def conversions {:name identity
+                  :glitter-index str->int})
+
+(defn convert
+  [vamp-key value]
+  ((get conversions vamp-key) value))
+
+(defn parse
+  "Convert a CSV into rows of columns"
+  [string]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split string #"\n")))
